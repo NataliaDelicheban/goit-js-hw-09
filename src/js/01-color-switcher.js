8560bff2444e91d93e4a1d329c7a7c9c2358d017
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-06-06 21:19:38 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', true);
+  //console.log('start');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+  //console.log('stop!');
+});
