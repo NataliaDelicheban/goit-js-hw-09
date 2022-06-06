5bc12c1ad27ee089020fb3ad63b41d8e7d177899
NataliaDelicheban <94997893+NataliaDelicheban@users.noreply.github.com>
2022-06-06 19:20:12 +0000

    Deploying to gh-pages from @ NataliaDelicheban/goit-js-hw-09@8560bff2444e91d93e4a1d329c7a7c9c2358d017 🚀

---
+++ 01-color-switcher.2be96ecf.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");var n=null;t.addEventListener("click",(function(){n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.2be96ecf.js.map
