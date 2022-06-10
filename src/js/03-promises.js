4591eecda1f94ea9d082574b4a139a8dcb80f5fb
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-06-10 17:36:12 +0200

    create task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('[name=delay]');
+const step = document.querySelector('[name=step]');
+const amount = document.querySelector('[name=amount]');
+
+let position = 0;
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+  setTimeout(() => {
+    for (let i = 0; i < amount.value; i += 1) {
+      const s = +delay.value + +step.value * i;
+      position = i;
+      createPromise(position, s)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+          //console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+          //console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  }, delay.value);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+  // return promise;
 }
